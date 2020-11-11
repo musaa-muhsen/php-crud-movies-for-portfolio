0ed3e3b7f4010e66e6881b1d0cc14c024c940e2b
musaa-muhsen <musaamuhsen@gmail.com>
2020-11-11 16:48:24 +0000

    functionality sorted need to add styling

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> -->
 </head>
 <body>
 <?php require_once 'process.php'; ?>
@@ -18,78 +18,72 @@
        echo $_SESSION['message'];
        unset($_SESSION['message']);
     ?>
-    </div>
+   
 <?php endif; ?>
 
+<form action="process.php" method="POST">
+   <input type="hidden" name="id" value="<?php echo $id; ?>">
 
-<div class="container">
+        <div class="">
+       <label>Title:</label>
+       <input type="text" name="name" class="" value="<?php echo $name; ?>" placeholder="Enter you name">
+       </div>
+
+       <div class="">
+       <label>Synopsis:</label>
+       <input type="text" name="location" class="" value="<?php echo $location; ?>"  placeholder="Enter your location">
+       </div>
+
+       <div class="">
+       <label>Seen on:</label>
+       <input type="text" name="seen" class="" value="<?php echo $seen; ?>"  placeholder="Enter your location">
+       </div>
+
+       <div class="">
+       <?php if ($update == true): ?>
+         <button class="" type="submit" name="update">Update</button>
+       <?php else: ?>
+           <button class="" type="submit" name="save">Save</button>
+       <?php endif; ?>
+       </div>
+
+   </form> 
+   
+
 <?php 
 
 // this becomes objected oriented???
 $mysqli = new mysqli('localhost', 'musaa', '1234', 'crud-tut-1') or die(mysqli_error($mysqli));
+
+if(!$mysqli) {
+    echo 'connection error' . mysqli_connect_error(); 
+ }
 
 // -> calls/sets object variables. // The object operator, ->, is used in object scope to access methods and properties of an object. It’s meaning is to say that what is on the right of the operator is a member of the object instantiated into the variable on the left side of the operator. Instantiated is the key term here.
 $result = $mysqli->query("SELECT * FROM data") or die($mysqli->error);
 // pre_r($result); //mysqli_result Object all the records from the database 
 //pre_r($result->fetch_assoc()); // prints out a record to the screen 
 ?>
-<div class="row justify-content-center">
-<table class="table">
-<thead>
-     <tr>
-          <th>Name</th>
-          <th>Location</th>
-          <th colspan="2">Action</th>
-      </tr>
-</thead>
+<div >
+
 <?php 
    while ($row = $result->fetch_assoc()): ?>
-
-   <tr>
-        <td><?php echo $row['name']; ?></td>
-        <td><?php echo $row['location']; ?></td>
-        <td>
-        <a href="index.php?edit=<?php echo $row['id']; ?>" class="btn btn-info">Edit</a>
-        <a href="process.php?delete=<?php echo $row['id']; ?>" class="btn btn-danger">Delete</a>
-        </td>
-   </tr>
-   <?php endwhile; ?>
-</table>
-</div>
+    <div>
+        <?php echo $row['name']; ?>
+        <?php echo $row['location']; ?>
+        <?php echo $row['seen']; ?>
+        </div> 
+        <a href="index.php?edit=<?php echo $row['id']; ?>" class="btn btn-info">✏️</a>
+        <a href="process.php?delete=<?php echo $row['id']; ?>" class="btn btn-danger">&#128465;
+</a>
+      
+    <?php endwhile; ?>
+   </div>
 <?php  
-
-// function for print_r 
-function pre_r($array) {
-    echo '<pre>';
-    print_r($array);
-    echo '</pre>';
-}
 
 ?>
 
-<div class="row justify-content-center">
-   <form action="process.php" method="POST">
-   <input type="hidden" name="id" value="<?php echo $id; ?>">
-   <div class="form-group">
-       <label>Name</label>
-       <input type="text" name="name" class="form-control" value="<?php echo $name; ?>" placeholder="Enter you name">
-       </div>
-       <div class="form-group">
-     
-       <label>Location</label>
-       <input type="text" name="location" class="form-control" value="<?php echo $location; ?>"  placeholder="Enter your location">
-       </div>
 
-       <div class="form-group">
-       <?php if ($update == true): ?>
-         <button class="btn btn-info" type="submit" name="update">Update</button>
-       <?php else: ?>
-           <button class="btn btn-primary" type="submit" name="save">Save</button>
-       <?php endif; ?>
-       </div>
 
-   </form> 
-   </div>
-   </div>
 </body>
 </html>
--- process.php
+++ process.php
@@ -4,38 +4,44 @@ session_start();
 
 $mysqli = new mysqli('localhost', 'musaa', '1234', 'crud-tut-1') or die(mysqli_error($mysqli));
 
-$name = $location = '';
+if(!$mysqli) {
+    echo 'connection error' . mysqli_connect_error(); 
+ }
+$name = $location = $seen = '';
 $update = false;
 
 // check if save has been pressed 
 if (isset($_POST['save'])) {
-    $name = $_POST['name'];
-    $location = $_POST['location'];
+    $name = $mysqli -> real_escape_string($_POST['name']);
+    $location = $mysqli -> real_escape_string($_POST['location']);
+    $seen = $mysqli -> real_escape_string($_POST['seen']);
 
  
     // insert into database 
     // The die() function is an alias of the exit() function.
-    $mysqli->query("INSERT INTO data (name, location) VALUES('$name', '$location')") or die($mysqli->error);
+    $mysqli->query("INSERT INTO data (name, location, seen) VALUES('$name', '$location', '$seen')") or die($mysqli->error);
     
 
-    $_SESSION['message'] = "Record has been saved!";
+    $_SESSION['message'] = "&#127902; saved!";
     $_SESSION['msg_type'] = "success";
 
     header("location: index.php");
 } 
 
+// delete 
 if (isset($_GET['delete'])) {
-    $id = $_GET['delete'];
+    $id = $mysqli -> real_escape_string($_GET['delete']);
     $mysqli->query("DELETE FROM data WHERE id=$id") or die($mysqli->error());
     
-    $_SESSION['message'] = "Record has been deleted!";
+    $_SESSION['message'] = "&#128680; Deleted!";
     $_SESSION['msg_type'] = "danger";
 
     header("location: index.php");
 }
 
+// edit 
 if (isset($_GET['edit'])) {
-    $id = $_GET['edit'];
+    $id = $mysqli -> real_escape_string( $_GET['edit']);
     $update = true;
     $result = $mysqli->query("SELECT * FROM data WHERE id=$id") or die($mysqli->error());
     // count($result does'nt work due to php version 
@@ -43,20 +49,21 @@ if (isset($_GET['edit'])) {
        $row = $result->fetch_array();
        $name = $row['name'];
        $location = $row['location'];
-
+       $seen = $row['seen'];
     }
      
 }
 
-
+// update 
 if (isset($_POST['update'])) {
-    $id = $_POST['id'];
-    $name = $_POST['name'];
-    $location = $_POST['location'];
+    $id = $mysqli -> real_escape_string($_POST['id']);
+    $name = $mysqli -> real_escape_string($_POST['name']);
+    $location = $mysqli -> real_escape_string($_POST['location']);
+    $seen = $mysqli -> real_escape_string($_POST['seen']);
 
-    $mysqli->query("UPDATE data SET name='$name', location='$location' WHERE id=$id") or die($mysqli->error); 
+    $mysqli->query("UPDATE data SET name='$name', location='$location', seen='$seen' WHERE id=$id") or die($mysqli->error); 
 
-    $_SESSION['message'] = "&#8986; Record has been updated!";
+    $_SESSION['message'] = "&#9989; Updated!";
     $_SESSION['msg_type'] = "warning";
 
     header('location: index.php');
