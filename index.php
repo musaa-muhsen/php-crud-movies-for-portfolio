93007df2df25cc68e5d9b3a1468741fa9459f278
musaa-muhsen <musaamuhsen@gmail.com>
2020-11-11 23:26:17 +0000

    finishing touches, put live and connect to siteground db, add incremention

--- index.php
+++ index.php
@@ -4,12 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./css/styles.css">
     <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> -->
 </head>
 <body>
 <?php require_once 'process.php'; ?>
+
+
 <?php  if(isset($_SESSION['message'])): 
     // It's a shorthand for <?php echo $_SESSION['msg']  >?  //It's enabled by default since 5.4 regardless of php.ini settings.
     ?>
@@ -18,32 +21,41 @@
        echo $_SESSION['message'];
        unset($_SESSION['message']);
     ?>
-   
+   </div>
 <?php endif; ?>
 
-<form action="process.php" method="POST">
+<header>
+    <div>	<h1><span class="cow-emoji">&#128004;</span>vies </h1></div>
+    <!-- <div></div>
+    <div> <h1><a class="header-btn" href="">GO TO &#127902;</a></h1></div> -->
+</header>
+
+
+<form class="form-container form__group field" action="process.php" method="POST">
    <input type="hidden" name="id" value="<?php echo $id; ?>">
 
-        <div class="">
-       <label>Title:</label>
-       <input type="text" name="name" class="" value="<?php echo $name; ?>" placeholder="Enter you name">
+        <div class="form-inputs form-inputs-1 ">
+       <!-- <label>Title:</label> -->
+       <input required type="text" name="name" class="form-single " value="<?php echo $name; ?>" placeholder="Title:">
+       </div>
+       
+
+       <div class="form-inputs form-inputs-2">
+       
+       <textarea class="plot form-single" placeholder="Plot:" name="location" rows="5" cols="40" value="<?php echo $location; ?>"></textarea>
+
        </div>
 
-       <div class="">
-       <label>Synopsis:</label>
-       <input type="text" name="location" class="" value="<?php echo $location; ?>"  placeholder="Enter your location">
+       <div class="form-inputs form-inputs-3">
+       <!-- <label>Seen on:</label> &#128190; // floppy desk -->
+       <input  type="text" name="seen" class="form-single seen-input" value="<?php echo $seen; ?>"  placeholder="Seen on:">
        </div>
 
-       <div class="">
-       <label>Seen on:</label>
-       <input type="text" name="seen" class="" value="<?php echo $seen; ?>"  placeholder="Enter your location">
-       </div>
-
-       <div class="">
+       <div class="submit-container">
        <?php if ($update == true): ?>
-         <button class="" type="submit" name="update">Update</button>
+        <div > <button   class="update-btn" type="submit" name="update">Update</button> </div>
        <?php else: ?>
-           <button class="" type="submit" name="save">Save</button>
+        <div> <button class="save-btn" type="submit" name="save">Save</button> </div>
        <?php endif; ?>
        </div>
 
@@ -82,6 +94,9 @@ $result = $mysqli->query("SELECT * FROM data") or die($mysqli->error);
 <?php  
 
 ?>
+<footer>
+   <p> Musaa Muhsen <?php echo date("Y"); ?> &copy; </p>
+   </footer>
 
 
 
